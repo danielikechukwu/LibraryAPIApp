eee6bf9bf6ac8a2810d0e426c70a4acf2cba7750
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-03 23:24:53 +0100

    Add project files.

---
+++ LibraryAPI/Controllers/BooksController.cs
@@ -0,0 +1,29 @@
+﻿using LibraryAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateBook([FromRoute] int id, [FromQuery] string category, [FromHeader(Name = "X-User-Id")] string userId, [FromBody] BookDetails details)
+        {
+            // Your logic to store the data (e.g., in a database)
+            // For demonstration, we'll just return the data in a response.
+            var response = new
+            {
+                BookId = id,
+                Category = category,
+                userId = userId,
+                Title = details.Title,
+                Author = details.Author,
+                Year = details.Year
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ LibraryAPI/Models/BookDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace LibraryAPI.Models
+{
+    public class BookDetails
+    {
+        public string Title { get; set; } 
+
+        public string Author { get; set; }
+
+        public int Year { get; set; }
+
+        public string Content { get; set; }
+
+    }
+}
